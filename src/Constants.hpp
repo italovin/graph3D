3f5cb814650f737c33069bcbd2e5962378d7cb12
Italo <hitalomacedo900@gmail.com>
2024-12-31 12:44:48 -0300

    Fix gitignore and some Constants type

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -6,7 +6,7 @@ namespace Constants
 {
     // Maximum number of objects to group in a single render group
     // This also defines the size of the uniform buffers in standard shaders
-    const unsigned long long maxObjectsToGroup = 8192;
+    const unsigned long maxObjectsToGroup = 8192;
 }
 
 #endif
